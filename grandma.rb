cb71c9b06a86a6c0238f1dac3d869db1bfe567f6
Jaime Johnsen <drivetraindiva@gmail.com>
2018-06-28 22:49:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -10,11 +10,10 @@
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
-require pry
+# 'I LOVE YOU TOO PUMPKIN
+
 def speak_to_grandma(phrase)
   if phrase == "Hi Nana, how are you?".upcase
-    binding.pry
     puts "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "WHAT DID YOU EAT TODAY?".upcase
     puts "NO, NOT SINCE 1938!"
