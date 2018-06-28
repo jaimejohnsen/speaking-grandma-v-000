731cd69abb30445d2e15b905a865d27357d22edb
Jaime Johnsen <drivetraindiva@gmail.com>
2018-06-28 23:13:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,11 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+    
   
- #   elsif phrase == "WHAT DID YOU EAT TODAY?".upcase
-  #  puts "WHAT!"
+    elsif phrase == "WHAT DID YOU EAT TODAY?".upcase
+    return "NO, NOT SINCE 1938!"
+  
   else phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
   end
